a900638b12c81406aef5ef2bfac699e89e997a87
Moritz Hauff <moritz.hauff@gmx.de>
2017-02-16 23:54:44 +0100

    Importieren der Fast-Digital-IO-Bibliothek.
    Reduzieren der benötigten Zeit um auf einen digitalen Pin des Arduino
    Megas zuzugreifen von jeweils ca. 6 us auf 0.2 us (Mikrosekunden).

--- SPEC_A_2/SPEC_A_2.ino
+++ SPEC_A_2/SPEC_A_2.ino
@@ -2,18 +2,24 @@
 ***
 *** Dieses Programm läuft auf dem SPECTATOR-Arduino und überwacht dessen Sensoren.
 ***
-*** // todo: Was funktioniert gerade?
+*** Test der schnelleren IO-Bibliothek von: 
+*** https://www.codeproject.com/Articles/732646/Fast-digital-I-O-for-Arduino
 ***
 *** Moritz Hauff, 16.02.2017
 **/
 
 ///////////////////////////////////////////////////////////////////////////
 ///Includes
-// todo: add necessary includes
+#define  GPIO2_PREFER_SPEED    1   // results in even faster execution
+#include <arduino2.h>   // include the fast I/O 2 functions
+
 
 ///////////////////////////////////////////////////////////////////////////
 ///Constants
-const int LED_Pin = 32;
+// The I/O 2 functions use special data type for pin
+// Pin codes, such as DP13 are defined in pins2_arduino.h
+const GPIO_pin_t led_pin = DP32;  // Achtung: Pins nur für ihren im Setup angegeben Zweck nutzen, sonst kann es zur Beschädigung des ATmegas2560 kommen!
+
 const int DELAY_TIME = 100;
 
 
@@ -21,15 +27,15 @@ const int DELAY_TIME = 100;
 ///Setup
 void setup()
 {
-	pinMode(LED_Pin, OUTPUT);
+	pinMode2f(led_pin, OUTPUT);
 }
 
 ///////////////////////////////////////////////////////////////////////////
 ///Loop
 void loop()
 {
-	digitalWrite(LED_Pin, HIGH);
+	digitalWrite2f(led_pin, HIGH);
 	delay(DELAY_TIME);
-	digitalWrite(LED_Pin, LOW);
+	digitalWrite2f(led_pin, LOW);
 	delay(DELAY_TIME);
 }
---
+++ archive/Readme.md
@@ -0,0 +1,25 @@
+# Archive
+
+Dieser Ordner enthält alle zusätzlich verwendeten Bibliotheken und SourceCodes.
+
+## dio2
+
+Diese Bibliothek dient dem schnelleren Ansteuern der Digital-IO-Pins des Arduinos.
+Mehr Informationen in der dio2_Explanation.pdf oder unter: 
+https://www.codeproject.com/Articles/732646/Fast-digital-I-O-for-Arduino
+
+
+
+## Credits
+
+Moritz Hauff, 16.02.2017
+
+TODO: Write credits
+
+
+
+## License
+
+
+
+TODO: Write license
