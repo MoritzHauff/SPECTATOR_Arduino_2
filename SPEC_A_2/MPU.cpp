d471d740f3d6b081736fd8a4b2a887cb4c782fa0
Moritz Hauff <moritz.hauff@gmx.de>
2017-02-17 22:42:19 +0100

    Anpassungen der MPU6050-Funktionalitäten.
    Das MPU6050 wird jetzt ebenfalls in einer Klasse gehandhabt.
    Außerdem wurden die zeitlichen Auswirkungen getesetet und festgestellt,
    dass das Abfragen des MPU bisher noch sehr lange braucht (10 ms!).
    
    Um die MPU6050-Klasse in einer anderen Klasse zu verwenden musste die
    MPU6050-Bibliothek angepasst werden. Diese bearbeitete Version findet
    sich im archive-Ordner dieses Repository.

---
+++ SPEC_A_2/MPU.cpp
@@ -0,0 +1,107 @@
+// 
+// 
+// 
+
+#include "MPU.h"
+
+MPU::MPU()
+{
+	dmpReady = false;
+}
+
+void MPU::Init()
+{
+	// initialize device
+	Serial.println(F("Initializing MPU-I2C ..."));
+	mpu.initialize();
+
+	// verify connection
+	Serial.println(F("Testing device connections..."));
+	Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+	//load and configure the DMP
+	Serial.println(F("Initializing DMP..."));
+	devStatus = mpu.dmpInitialize();
+
+	// supply your own gyro offsets here, scaled for min sensitivity
+	mpu.setXGyroOffset(220);
+	mpu.setYGyroOffset(76);
+	mpu.setZGyroOffset(-85);
+	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+							   // make sure it worked (returns 0 if so)
+	if (devStatus == 0) {
+		// turn on the DMP, now that it's ready
+		Serial.println(F("Enabling DMP..."));
+		mpu.setDMPEnabled(true);
+
+		// enable Arduino interrupt detection
+		Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+		mpuIntStatus = mpu.getIntStatus();
+
+		// set our DMP Ready flag so the main loop() function knows it's okay to use it
+		Serial.println(F("DMP ready! Waiting for first interrupt..."));
+		dmpReady = true;
+
+		// get expected DMP packet size for later comparison
+		packetSize = mpu.dmpGetFIFOPacketSize();
+	}
+	else {
+		// ERROR!
+		// 1 = initial memory load failed
+		// 2 = DMP configuration updates failed
+		// (if it's going to break, usually the code will be 1)
+		Serial.print(F("DMP Initialization failed (code "));
+		Serial.print(devStatus);
+		Serial.println(F(")"));
+	}
+}
+
+void MPU::Update()
+{
+	// if programming failed, don't try to do anything
+	if (!dmpReady) return;
+
+	// reset interrupt flag and get INT_STATUS byte
+	//mpuIntStatus = mpu.getIntStatus();
+
+	// get current FIFO count
+	fifoCount = mpu.getFIFOCount();
+
+	// check for overflow (this should never happen unless our code is too inefficient)
+	if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+		// reset so we can continue cleanly
+		mpu.resetFIFO();
+		Serial.println(F("FIFO overflow!"));
+
+		// otherwise, check for DMP data ready interrupt (this should happen frequently)
+	}
+	else /*if (mpuIntStatus & 0x02) */
+	{
+		// wait for correct available data length, should be a VERY short wait
+		while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+		// read a packet from FIFO
+		mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+		// track FIFO count here in case there is > 1 packet available
+		// (this lets us immediately read more without waiting for an interrupt)
+		fifoCount -= packetSize;
+
+		/*Serial.print("loop[ms]: ");
+		Serial.print(zwei - drei);
+		Serial.print("\t");*/
+
+		// display Euler angles in degrees
+		mpu.dmpGetQuaternion(&q, fifoBuffer);
+		mpu.dmpGetGravity(&gravity, &q);
+		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+		Serial.print("ypr\t");
+		Serial.print(ypr[0] * 180 / M_PI);
+		Serial.print("\t");
+		Serial.print(ypr[1] * 180 / M_PI);
+		Serial.print("\t");
+		Serial.println(ypr[2] * 180 / M_PI);
+	}
+
+}
---
+++ SPEC_A_2/MPU.h
@@ -0,0 +1,72 @@
+// MPU.h - Moritz Hauff - 17.02.2017
+
+///////////////////////////////////////////////////////////////////////////
+/// Copyright (C) {2017}  {Moritz Hauff}
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/// If you start using parts of this code please write a short message to: 
+/// license@vierradroboter.de
+///
+/// If you have any questions contact me via mail: admin@vierradroboter.de
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef _MPU_h
+#define _MPU_h
+
+///////////////////////////////////////////////////////////////////////////
+///Includes
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+//#include <MPU6050_6Axis_MotionApps20.h>
+#include "MPU6050_2.h" // import *new* MPU6050.h with MotionApps already included
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+class MPU
+{
+protected: 
+	MPU6050 mpu = MPU6050();
+
+	// MPU control/status vars
+	bool dmpReady = false;  // set true if DMP init was successful
+	uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+	uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+	uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+	uint16_t fifoCount;     // count of all bytes currently in FIFO
+	uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+	// orientation/motion vars
+	Quaternion q;           // [w, x, y, z]         quaternion container
+	VectorFloat gravity;    // [x, y, z]            gravity vector
+	float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+public: 
+	MPU();
+	/*todo*/
+	void Init();
+	/*todo*/	
+	void Update();
+};
+
+#endif
+
