44da121e283978a13b59eecbc3fafcf25852cd95
Moritz Hauff <moritz.hauff@gmx.de>
2017-03-19 14:02:12 +0100

    Einfügen der MPUFahrer-Dateien in das WinkelFktTest-Projekt.

--- WinkelFktTest/main.cpp
+++ WinkelFktTest/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+#include "MPUFahrer.h"
+
 using namespace std;
 
 int main()
 {
     cout << "WinkelFktTest-Programm." << endl;
+
+    MPUFahrerClass mpuFahrer = MPUFahrerClass();
 
     return 0;
 }
