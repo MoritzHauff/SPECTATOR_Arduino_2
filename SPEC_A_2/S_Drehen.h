79e6b5a7eb0020ab4d626a244951d7331cd8aaa4
Moritz Hauff <moritz.hauff@gmx.de>
2017-04-11 16:52:11 +0200

    Kalibrieren der Drehwerte.
    Anpassen der Statemachine, damit diese inhct immer wieder den Modus
    	s_Drehen neu startet.

--- SPEC_A_2/S_Drehen.h
+++ SPEC_A_2/S_Drehen.h
@@ -39,10 +39,10 @@
 ///Konstanten
 const int S_Drehen_Timer = 3000;   // Nach wie vielen ms das Drehen mit einer Warnung beednet werden soll.
 
-const int S_Drehen_MinSpeed = 80;  // Die niedrigste Geschwindigkeit bei denen die Motoren den SPECTATOR noch bewegen.
+const int S_Drehen_MinSpeed = 82;  // Die niedrigste Geschwindigkeit bei denen die Motoren den SPECTATOR noch bewegen.
 const int S_Drehen_MaxSpeed = 180;  // Die höchste Motorgeschwindigkeit beim Drehen.
 
-const int S_Drehen_MotorSteigung = 300;  // Mit welcher Steigung die Motorgeschwindigkeit mit größerem MotorAbstand zunimmt.
+const int S_Drehen_MotorSteigung = 320;  // Mit welcher Steigung die Motorgeschwindigkeit mit größerem MotorAbstand zunimmt.
 
 ///////////////////////////////////////////////////////////////////////////
 ///State-Class
