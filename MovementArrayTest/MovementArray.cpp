8f3319e4ab975b49676bb233a4269b6661816754
Moritz Hauff <moritz.hauff@gmx.de>
2017-04-27 16:56:05 +0200

    Erstellen eines Testprojekts für den MovementArray.
    Einfügen der MovementArray-Klasse in das Spectator-Projekt.
    	Austesten eines MovementArrays.

---
+++ MovementArrayTest/MovementArray.cpp
@@ -0,0 +1,33 @@
+/** MovementArray.cpp
+***
+*** Diese Klasse verwaltet und erstellt eine List an Bewegungen, welche
+*** zum Beispiel dem Auswichen von Hindernissen dienen kann.
+***
+*** Moritz Hauff, 27.04.2017
+**/
+
+///////////////////////////////////////////////////////////////////////////
+///Includes
+#include "MovementArray.h"
+
+///////////////////////////////////////////////////////////////////////////
+///Instanz
+MovementArrayClass MovementArray;
+
+///////////////////////////////////////////////////////////////////////////
+///Funktionen
+void MovementArrayClass::GoToStart()
+{
+	aktPos = 0;
+}
+
+MotorDaten* MovementArrayClass::GetNextMovement(int Movements[][2], int Rows)
+{
+	if (aktPos < Rows-1 && aktPos >= -1)
+	{
+		aktPos++;
+		return new MotorDaten(Movements[aktPos][0], Movements[aktPos][1]);
+	}
+
+	return NULL;
+}
---
+++ MovementArrayTest/MovementArray.h
@@ -0,0 +1,50 @@
+// MovementArray.h - Moritz Hauff - 27.04.2017
+
+#ifndef _MOVEMENTARRAY_h
+#define _MOVEMENTARRAY_h
+
+///////////////////////////////////////////////////////////////////////////
+///Includes
+#include <iostream>
+
+///////////////////////////////////////////////////////////////////////////
+///MoterDaten-Struct
+struct MotorDaten
+{
+	int MotorSpeedL;
+	int MotorSpeedR;
+
+	MotorDaten(int MotorSpeedL = 0, int MotorSpeedR = 0)
+	{
+		this->MotorSpeedL = MotorSpeedL;
+		this->MotorSpeedR = MotorSpeedR;
+	}
+};
+
+///////////////////////////////////////////////////////////////////////////
+///ScriptedMovement-Class
+/*Stellt Funktionen zur bearbeitung von Movement Arrays bereit.*/
+class MovementArrayClass
+{
+ protected:
+	 int aktPos;
+
+ public:
+	 /// <summary>
+	 /// Setzt die aktuelle Array-Position auf die erste Bewegung und beginnt
+	 /// so mit der Liste von Bewegungen von vorne.
+	 /// </summary>
+	 void GoToStart();
+
+	 /// <summary>
+	 /// Gibt die in der Liste nächste Bewegung zurück und wandert mit der
+	 /// aktuellen Array-Postion eine Stelle weiter. Gibt null zurück, wenn keine weitere
+	 /// Bewegung mehr geplant ist.
+	 /// </summary>
+	 MotorDaten* GetNextMovement(int Movements[][2], int Rows);
+};
+
+extern MovementArrayClass MovementArray;
+
+#endif
+
