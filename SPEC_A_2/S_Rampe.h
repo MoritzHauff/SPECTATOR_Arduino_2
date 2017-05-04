1e126464a51bb58ba7b1954682636d9d5fbd1cb2
Moritz Hauff <moritz.hauff@gmx.de>
2017-05-04 18:45:47 +0200

    Weitere VErsuch um bei Bumpern auf der Rampe nicht zu kippen.

--- SPEC_A_2/S_Rampe.h
+++ SPEC_A_2/S_Rampe.h
@@ -21,7 +21,7 @@ class S_RampeClass : public StateClass
 ///////////////////////////////////////////////////////////////////////////
  protected: ///Konstanten
 	 const static int S_Rampe_USVorne = 7;
-	 const static int S_Rampe_NormalSpeed = 140;
+	 const static int S_Rampe_NormalSpeed = 120; // 140 kippt bei hindernissen  // 120 sehr langsam kippt aber nicht immer.
  
  protected:
 	 int stoppWahrscheinlichkeit;   // todo move to StateClass
