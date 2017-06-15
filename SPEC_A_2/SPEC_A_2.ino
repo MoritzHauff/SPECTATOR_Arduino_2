3267c02739bdc5a83070d48c20f96653e1cb7c77
Moritz Hauff <moritz.hauff@gmx.de>
2017-06-15 09:38:14 +0200

    Ändern des Headers der Hauptdatei.

--- SPEC_A_2/SPEC_A_2.ino
+++ SPEC_A_2/SPEC_A_2.ino
@@ -1,14 +1,43 @@
 /** SPEC_A_2.ino
 ***
-*** Dieses Programm läuft auf dem SPECTATOR-Arduino und überwacht dessen Sensoren.
+*** Dieses Programm läuft auf dem SPECTATOR-Arduino, überwacht und verarbeitet 
+*** dessen Sensoren.
 ***
-*** Test der schnelleren IO-Bibliothek. Und angeschlossener SHARP-Distanzsensoren,
-*** sowie des MPU6050s. ZeitTests der MLX-Temp-Sensoren und der Stoßsensoren Vorne.
+*** Für sämtliche Sensoren besteht mindestens eine Klasse zur Aufnahme der 
+*** Daten und zur Speicherung. Diese sind in den Filtern "Header Files - Sensors"
+*** und "Source Files - Sensors" geordnet.
+*** Die allgemeine Steuerung der Handlungen und die Auswahl welche Sensoren 
+*** ausgelesen werden sollen übernimmt eine StateMachine. "Header Files - States"
+*** und "Source Files - States".
+*** 
 ***
-*** Serielle Übertragung aller Sensorwerte und Steuerung der Motoren.
+*** Es besteht die Möglichkeit einer serielle Übertragung aller Sensorwerte und 
+*** Steuerung der Motoren an einen angeschlossen RaspberryPi oder andere PCs.
 ***
 *** Moritz Hauff, 01.03.2017
 **/
+
+///////////////////////////////////////////////////////////////////////////
+/// Copyright (C) {2017}  {Moritz Hauff}
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/// If you start using parts of this code please write a short message to: 
+/// license@vierradroboter.de
+///
+/// If you have any questions contact me via mail: admin@vierradroboter.de
+///////////////////////////////////////////////////////////////////////////
 
 // todo warum werden nachwievor zwei opfer angezeigt und versorgt?
 // todo drehfunktion anpassen, damit diese nicht mehr stuckt -> erhöhe geschw im laufe der vergangenen Zeit
@@ -16,7 +45,6 @@
 
 ///////////////////////////////////////////////////////////////////////////
 ///Includes
-#include "S_OpferAbwurf.h"
 #include "SPECTATORClass.h"
 #include "Overwatcher.h"
 
@@ -24,7 +52,6 @@
  
 ///////////////////////////////////////////////////////////////////////////
 ///Variablen
-
 unsigned long eins = 0;
 unsigned long zwei = 0;
 unsigned long drei = 0;
@@ -35,8 +62,10 @@ StateMachineClass *stateMachine;
 
 ///////////////////////////////////////////////////////////////////////////
 ///SRAM-Funktion
-/*Diese Funktion gibt die Menge des aktuell unbenutzten SRAMs zurück. 
-Für weitere Informationen siehe: Adafruit.*/
+/// <summary>
+/// Diese Funktion gibt die Menge des aktuell unbenutzten SRAMs zurück. 
+/// Für weitere Informationen siehe: https://learn.adafruit.com/memories-of-an-arduino/measuring-free-memory*/.
+/// </summary>
 int freeRam()
 {
 	extern int __heap_start, *__brkval;
